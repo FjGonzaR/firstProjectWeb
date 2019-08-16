cd4ba761fc7c5d1de921bc092c12bcfe9cdb16c9
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-08-16 06:33:14 -0500

    Avanzando ando

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,3 @@
-$(window).scroll(function(){
-    var position = $(this).scrollTop();
-    if(position >= 20) {
-      $('.principal-navbar').addClass('custom-navbar');
-    } else {
-      $('.principal-navbar').removeClass('custom-navbar');
-    }
-  });
-
   $(function() {
     var text = $(".text");
     $(window).scroll(function() {
