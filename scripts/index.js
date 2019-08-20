d2816ff783cb0aacb57516b432ba9984db78317d
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-08-19 21:05:13 -0500

    Navbar fixed

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,8 @@
   $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
   });
+/* para colapsar el navbar cuando se presiona un item */
+
+  $('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
